0d7064a6d97e1c2056e0143380006de75033a81c
Ian Liu Rodrigues <ian.liu@gebrproject.com>
2013-03-19 19:20:50 -0300

    Remove get_scatter_info from histogram project

--- histogram/main.c
+++ histogram/main.c
@@ -1,3 +1,4 @@
+#include <common.h>
 #include <limits.h>
 #include <math.h>
 #include <mpi.h>
@@ -49,22 +50,6 @@ int *get_bucket_cache(int len)
 	int *cache = malloc(sizeof(int) * len);
 	memset(cache, 0, sizeof(int) * len);
 	return cache;
-}
-
-/**
- * Fills the @sendcnts and @displs vectors to scatter the data.
- */
-void get_scatter_info(int *sendcnts, int *displs, int n_ranks, int n)
-{
-	int sum = 0;
-	int a = n / n_ranks;
-	int b = n - a*n_ranks;
-	for (int i = 0; i < n_ranks; i++) {
-		sendcnts[i] = a + (b > 0? 1:0);
-		displs[i] = sum;
-		sum += sendcnts[i];
-		b--;
-	}
 }
 
 /**
