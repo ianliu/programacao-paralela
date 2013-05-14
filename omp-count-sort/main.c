8839c763c71a3b4bf56960bd62f902d558acb9fb
Ian Liu Rodrigues <ian.liu88@gmail.com>
2013-05-14 13:19:37 -0300

    Add options for omp-count-sort

--- omp-count-sort/main.c
+++ omp-count-sort/main.c
@@ -1,7 +1,13 @@
 #include <omp.h>
+#include <profiler.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+static int NUM_POINTS = 0;
+static bool USE_QSORT = false;
 
 void count_sort(int a[], int n)
 {
@@ -32,16 +38,68 @@ void print_v(int a[], int n)
 		printf("%d\n", a[i]);
 }
 
+void usage(char *prog)
+{
+	fprintf(stderr, "Usage: %s -h | -n NUM_POINTS [-q]\n", prog);
+	fprintf(stderr, "    -n      Number of points to read from stdin.\n");
+	fprintf(stderr, "    -q      Use qsort method instead.\n");
+	exit(1);
+}
+
+void get_options(int argc, char *argv[])
+{
+	int opt;
+	while ((opt = getopt(argc, argv, "hn:q")) != -1) {
+		switch (opt) {
+			case 'h':
+				usage(argv[0]);
+				break;
+			case 'n':
+				NUM_POINTS = atoi(optarg);
+				break;
+			case 'q':
+				USE_QSORT = true;
+				break;
+			default:
+				usage(argv[0]);
+				break;
+		}
+	}
+
+	if (NUM_POINTS <= 0)
+		usage(argv[0]);
+}
+
+int cmp(const void *aa, const void *bb)
+{
+	int a = *(int*)aa;
+	int b = *(int*)bb;
+	return a - b;
+}
+
 int main(int argc, char *argv[])
 {
 	int n, *a;
+	Profiler prof_sort;
+	profiler_init(&prof_sort, "sort");
 
-	n = atoi(argv[1]);
+	get_options(argc, argv);
+
+	n = NUM_POINTS;
 	a = malloc(n*sizeof(int));
 	for (int i = 0; i < n; i++)
 		scanf("%d", &a[i]);
 
-	count_sort(a, n);
+	profiler_start(&prof_sort);
+	if (USE_QSORT)
+		qsort(a, n, sizeof(int), cmp);
+	else
+		count_sort(a, n);
+	profiler_stop(&prof_sort);
+
+	fprintf(stderr, "%" PRIu64 "\n", prof_sort.total);
+	profiler_free(&prof_sort);
+
 	print_v(a, n);
 
 	return 0;
